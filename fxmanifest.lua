79baccabb571ff7b338fd4cec2fd06d90f69aa9f
CoyoteCarnevale <xaniahlemaitre@gmail.com>
2026-02-13 16:17:53 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+games { 'rdr3' }
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+description 'Prevents peds from spawning serverwide.'
+version '1.0.0'
+author 'CoyoteCarnevale'
+
+client_scripts {
+    'client.lua'
+}
+
+lua54 'yes'
