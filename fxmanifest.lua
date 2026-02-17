7a57c1a6e660b0a5a4d5a6988279617896710628
CoyoteCarnevale <xaniahlemaitre@gmail.com>
2026-02-16 20:51:16 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,11 +3,15 @@ games { 'rdr3' }
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 description 'Prevents peds from spawning serverwide.'
-version '1.0.0'
+version '1.0.2'
 author 'CoyoteCarnevale'
 
-client_scripts {
-    'client.lua'
+shared_scripts {
+    'config.lua'
+}
+
+server_scripts {
+    'server.lua'
 }
 
 lua54 'yes'
