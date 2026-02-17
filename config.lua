3a221867dbfe43f4785861b19afbd3df79965058
CoyoteCarnevale <xaniahlemaitre@gmail.com>
2026-02-16 23:43:29 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,12 +5,12 @@ Config.Debug = false
 
 -- Add any peds that you don't want to spawn in your server here!
 Config.BlockedPeds = {
-    "a_m_y_sdstreetkids_slums_02",
-    "cs_sd_streetkid_01",
-    "cs_sd_streetkid_01a",
-    "cs_sd_streetkid_01b",
-    "cs_sd_streetkid_02",
-    "cs_sdstreetkidthief",
-    "a_m_y_nbxstreetkids_01",
-    "a_m_y_nbxstreetkids_slums_01",
+    [`a_m_y_sdstreetkids_slums_02`] = true,
+    [`cs_sd_streetkid_01`] = true,
+    [`cs_sd_streetkid_01a`] = true,
+    [`cs_sd_streetkid_01b`] = true,
+    [`cs_sd_streetkid_02`] = true,
+    [`cs_sdstreetkidthief`] = true,
+    [`a_m_y_nbxstreetkids_01`] = true,
+    [`a_m_y_nbxstreetkids_slums_01`] = true
 }
