21b86425be71298345a6065081417d2107941cdc
CoyoteCarnevale <xaniahlemaitre@gmail.com>
2026-02-16 20:50:28 -0500

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,16 @@
+Config = {}
+
+-- Toggle debug prints (will show a notification in your server console whenever a ped is blocked from spawning.)
+Config.Debug = false
+
+-- Add any peds that you don't want to spawn in your server here!
+Config.BlockedPeds = {
+    "a_m_y_sdstreetkids_slums_02",
+    "cs_sd_streetkid_01",
+    "cs_sd_streetkid_01a",
+    "cs_sd_streetkid_01b",
+    "cs_sd_streetkid_02",
+    "cs_sdstreetkidthief",
+    "a_m_y_nbxstreetkids_01",
+    "a_m_y_nbxstreetkids_slums_01",
+}
